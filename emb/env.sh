02356868ea9ff6d8095e88c0e38b6c32c87e47df
sifanli <johnsifanli@gmail.com>
2025-09-08 07:07:02 +0000

    add env.sh

---
+++ emb/env.sh
@@ -0,0 +1,3 @@
+pip install torch torchvision torchaudio
+pip install -U transformers accelerate sentencepiece bitsandbytes
+# if you use the official LLaVA repo you can pip install llava; here we illustrate with HuggingFace API
